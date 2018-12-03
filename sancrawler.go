16f60386d42a401164701eebee96301a19060c8a
Peter Crampton <pwc42@drexel.edu>
2018-12-02 19:54:38 -0500

    Attempt at fixing bug with timeouts. Added additional documentation.

--- sancrawler.go
+++ sancrawler.go
@@ -30,20 +30,43 @@ type caID struct {
 	NumCerts int `json:"num_certs"`
 }
 
+/* crawlerFn: Goroutine crawler function. Regexs can be shared safely between
+ * goroutines as long as they aren't changing, which ours don't. So, we use 1
+ * regex struct for each type we want to extract and pass pointers to it for
+ * efficiency.
+ */
 func crawlerFn(commonNameRegex *regexp.Regexp, sanRegex *regexp.Regexp) {
 	for {
 		select {
+		// Once this case becomes avaliable, it means that there are no more IDs
+		// to process, so we can exit. Other goroutines may still be processing the
+		// remaining IDs.
 		case <-doneChan:
 			return
 
+		// New ID to process?
 		case id := <-idChan:
 			url := crtshBase + id
 			res, err := http.Get(url)
 
 			if err != nil {
-				panic(err)
+				// BUG: crt.sh seems to be imposing some kind of undocumented rate-limiting. These
+				// lines have solved the problem for me, experimentally.
+				time.Sleep(5 * time.Second)
+				res, err = http.Get(url)
+				if err != nil {
+					// Bail out if that doesn't work.
+					panic(err)
+				}
 			}
 
+			// We use regexs to extract what we want from the output of the decoded certificate.
+			// The certificate is shown in a format originally derived from this RFC:
+			// https://tools.ietf.org/html/rfc1422#appendix-A
+			//
+			// So, because we are processing an extended version of that spec which happens to
+			// exist inside HTML, we can use regexs instead of a full HTML parser. This has
+			// tremendous performance gains without any risk.
 			body, _ := ioutil.ReadAll(res.Body)
 			bodys := string(body)
 			commonNames := commonNameRegex.FindAllString(bodys, -1)
@@ -54,14 +77,14 @@ func crawlerFn(commonNameRegex *regexp.Regexp, sanRegex *regexp.Regexp) {
 			// Ignore the first common name since that is the common name of the issuer.
 			if commonNames != nil {
 				for i := 1; i < len(commonNames); i++ {
-					// Remove commonName= and <
+					// Remove "commonName=" and "<"
 					nameChan <- commonNames[i][11 : len(commonNames[i])-1]
 				}
 			}
 
 			if san != nil {
 				for i := 0; i < len(san); i++ {
-					// Remove DNS: and <
+					// Remove "DNS:" and "<"
 					nameChan <- san[i][4 : len(san[i])-1]
 				}
 			}
@@ -69,11 +92,17 @@ func crawlerFn(commonNameRegex *regexp.Regexp, sanRegex *regexp.Regexp) {
 	}
 }
 
-// The JSON coming back from crt.sh endpoints is not properly formatted. Thus,
-// we have to make some changes to it in order for it to actually be parseable.
+/* patchJSON: The JSON coming back from the crt.sh is not properly formatted. Thus,
+ * we have to make some changes to it in order for it to actually be parseable all
+ * in one go.
+ */
 func patchJSON(jsondata []byte) string {
 	var builder strings.Builder
 	balanced := 0
+
+	// All we really do here is add commas between the individual records that
+	// crt.sh returned to us. Idk if there is a better way, but this seems to
+	// be fast enough.
 
 	builder.Grow(len(jsondata))
 	builder.WriteByte('[')
@@ -94,6 +123,16 @@ func patchJSON(jsondata []byte) string {
 	return builder.String()
 }
 
+/* getNames: Gets the names, ie. domain/subdomain names from certificates that are
+ * returned from searching crt.sh for certificates matching a certain search URL
+ * with a certain HTTP GET parameter
+ *
+ * Higher-level functions (see below) use this by passing a url that corresponds to
+ * the specific endpoint we retrieve our search query results from. Since the output
+ * format is always the same, we can use this function to parse the names, no matter
+ * what the actual search type is. This makes it easy to add additional features
+ * later, if desired.
+ */
 func getNames(url string, httpParam string, numCrawlers int) map[string]int {
 	certIDRegex, _ := regexp.Compile(`\?id\=\d+`)
 	groupedCAIDs := []caID{}
@@ -107,6 +146,8 @@ func getNames(url string, httpParam string, numCrawlers int) map[string]int {
 
 	defer res.Body.Close()
 
+	// Lemme patch that JSON real quick
+
 	body, _ := ioutil.ReadAll(res.Body)
 	caJSON := patchJSON(body)
 
@@ -115,15 +156,28 @@ func getNames(url string, httpParam string, numCrawlers int) map[string]int {
 		return nil
 	}
 
+	// We grouped by CAID because this results in better performance overall. Crt.sh will
+	// aggregate results into a table grouped by their CAID rather than return all matching
+	// results. This is basically to prevent large organizations from timing out.
+
 	for i := 0; i < len(groupedCAIDs); i++ {
+		// Hey man, its go, and when its go, you let it do what go does. CONCURRENCY.
 		go (func(ca caID) {
+			// Each CA may have issued several hundreds certs, so in the future, we may have to
+			// actually paginate these results. Right now, we do a performance hack and try to get
+			// all the results in one go. So we tell it to return 1 page with all the issued certs.
 			caURL := fmt.Sprintf("https://crt.sh/?icaid=%d&p=1&n=%d&%s", ca.ID, ca.NumCerts, httpParam)
 			res, err := http.Get(caURL)
 
+			// Bail out if shit goes south
 			if err != nil {
 				panic(err)
 			}
 
+			// I don't feel guilty about this one either. Performance feels too good man. Also, we
+			// never ACTUALLY parse the HTML, we just look for patterns of text that correspond to IDs,
+			// and this text happens to be inside HTML. Unless the site changes dramatically, I think
+			// its gonna be fine.
 			caBody, _ := ioutil.ReadAll(res.Body)
 			ids := certIDRegex.FindAllString(string(caBody), -1)
 
@@ -137,6 +191,8 @@ func getNames(url string, httpParam string, numCrawlers int) map[string]int {
 		})(groupedCAIDs[i])
 	}
 
+	// Concurrency is fun
+
 	count := 0
 	for count != len(groupedCAIDs) {
 		select {
@@ -147,7 +203,16 @@ func getNames(url string, httpParam string, numCrawlers int) map[string]int {
 		}
 	}
 
-	fmt.Printf("Processing %d IDs with %d goroutines . . . ", len(idChan), numCrawlers)
+	fmt.Printf("\nProcessing %d IDs with %d goroutines . . . ", len(idChan), numCrawlers)
+
+	// At this point, we have a bunch of IDs corresponding to different endpoints on crt.sh
+	// which we are going to process and extract names from. These IDs came from the search
+	// query we just performed, optimized by grouping by CAID to try to prevent the result
+	// from timing out. But what we got from the step above was just the IDs, not the actual
+	// entires. Some of these IDs are duplicates in terms of names, but there is no way
+	// to filter across that as far as I know in crt.sh
+
+	// Our single regex structs shared with all goroutines
 
 	commonNameRegex := regexp.MustCompile(`(?i)commonName=.*?<`)
 	sanRegex := regexp.MustCompile(`(?i)DNS:.*?<`)
@@ -191,6 +256,9 @@ func getNames(url string, httpParam string, numCrawlers int) map[string]int {
 	return names
 }
 
+/* getDomainsByOrg: Get all the names belonging to a certain organization name.
+ * Also use a specified number of "crawlers" which are goroutines.
+ */
 func getDomainsByOrg(orgname string, numCrawlers int) map[string]int {
 	orgname = url.PathEscape(orgname)
 	url := crtshBase + crtshOrg + orgname
@@ -198,6 +266,8 @@ func getDomainsByOrg(orgname string, numCrawlers int) map[string]int {
 	return getNames(url, httpParam, numCrawlers)
 }
 
+/* getDomainsByKeyword: General search, any match on any fields in x509 spec.
+ */
 func getDomainsByKeyword(keyword string, numCrawlers int) map[string]int {
 	keyword = url.PathEscape(keyword)
 	url := crtshBase + crtshKeyword + keyword
@@ -205,21 +275,22 @@ func getDomainsByKeyword(keyword string, numCrawlers int) map[string]int {
 	return getNames(url, httpParam, numCrawlers)
 }
 
-func printASCIIArt() {
+/* ayy */
+func printASCIIArt(major int, minor int) {
 	art := `
   __________
-  \\        | SAN CRAWLER v2: Electric Boogaloo
+  \\        | SAN CRAWLER v%d.%d: Uncle Rico's Time Machine
    \\       |    @cramppet
     \\@@@@@@|   
   `
-	fmt.Println(art)
+	fmt.Printf(art, major, minor)
 }
 
 func main() {
 	var keyword = flag.String("k", "", "Keyword to match on. Be careful with wildcard.")
 	var org = flag.String("s", "", "Organization name to match on. Be careful with wildcard.")
 	var print = flag.Bool("p", false, "Print results to stdout")
-	var threads = flag.Int("t", 75, "Number of goroutines to use")
+	var threads = flag.Int("t", 50, "Number of goroutines to use")
 	var outfile = flag.String("o", "", "Output file")
 	flag.Parse()
 
@@ -227,7 +298,7 @@ func main() {
 	nameChan = make(chan string, maxChanLen)
 	doneChan = make(chan bool, *threads)
 
-	printASCIIArt()
+	printASCIIArt(2, 1)
 	subdomains := make(map[string]int)
 	start := time.Now()
 
@@ -237,6 +308,7 @@ func main() {
 		subdomains = getDomainsByOrg(*org, *threads)
 	}
 
+	// Why not show this bad motherfucker off?
 	elapsed := time.Since(start)
 
 	if *print {
